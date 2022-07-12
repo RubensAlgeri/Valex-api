bef88d6567db1c418160949bb5c2760170703ea3
Rubens Algeri <rubens_zys@hotmail.com>
2022-07-12 03:28:30 -0300

    style: implemented a ssl config on database connection

--- config/database.ts
+++ config/database.ts
@@ -4,10 +4,16 @@ dotenv.config();
 
 const {Pool} = pg;
 
-const databaseConfig = {
-    connectionString: process.env.DATABASE_URL,
-};
+const connectionsConfig:pg = {
+    connectionString: process.env.DATABASE_URL
+}
 
-const connection = new Pool(databaseConfig);
+if (process.env.MODE === 'PROD'){
+    connectionsConfig.ssl = {
+    rejectUnauthorized: false
+    }
+}
+
+const connection = new Pool(connectionsConfig);
 
 export default connection;
